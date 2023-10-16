7b1c626b25588e94e5a9550d8a52e65ad35e6d58
JulianC <jacaminaur@gmail.com>
2023-10-16 20:58:06 +0200

    counter now functional

--- script.js
+++ script.js
@@ -1,10 +1,14 @@
 
 const notifications = document.querySelectorAll('.notification');
 const removeAllBtn = document.getElementById('removeall');
+counter = document.getElementById('counter');
+
 
 for (const notification of notifications) {
   notification.addEventListener('click',(e)=>{
     notification.classList.remove('unread');
+    counter.dataset.value = (counter.dataset.value>0) ? counter.dataset.value -1 : 0;
+    setCounter();
   });
 }
 
@@ -12,4 +16,10 @@ removeAllBtn.addEventListener('click',(e)=>{
   for (const notification of notifications) {
       notification.classList.remove('unread');
   }
+  counter.dataset.value = 0;
+  setCounter();
 });
+
+function setCounter(){
+  counter.innerHTML = counter.dataset.value;
+}
